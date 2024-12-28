0255b15e6b277d7727a60a1ab824f1cba6f06d3a
David Hua <dh-alex@outlook.com>
2024-12-28 00:27:32 -0800

    Turn api method into a script that can get a list of department

---
+++ backend/department_list_extraction.py
@@ -0,0 +1,14 @@
+import requests
+import pandas as pd
+
+def fetch_department_list():
+    # Fetch all course data
+    response = requests.get("https://ubcexplorer.io/getAllCourses")
+    all_courses = response.json()
+
+    df_courses = pd.DataFrame(all_courses)
+
+    return df_courses[["dept","cred"]].groupby("dept").mean().reset_index()["dept"].tolist()
+
+department_list = fetch_department_list()
+print(department_list)
